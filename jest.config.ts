0180ed3dff8387b54d7ede1d6c088f6ff74213c7
HenrickyL <Henrickyl1@gmail.com>
2022-10-20 13:09:28 -0300

    chore: add ts-node-dev dependency and ajust path to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@
  */
 
 export default {
-  roots: ['<rootDir>/src>'],
+  roots: ['<rootDir>/src'],
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
@@ -12,10 +12,10 @@ export default {
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
+  testEnvironment: 'node',
 
   // Indicates which provider should be used to instrument code for coverage
   // coverageProvider: "v8",
-
   // A map from regular expressions to paths to transformers
   transform: {
     '.+\\.ts$': 'ts-jest'
